c555308fc8208746f39fadce13b4be9094b64ead
Daniel Martins <imthedaniels@gmail.com>
2023-07-16 20:01:45 -0300

    test: ensure SignUpController returns 400 if no name is provided

--- jest.config.ts
+++ jest.config.ts
@@ -1,16 +1,14 @@
-import type { Config } from 'jest';
+import type { Config } from 'jest'
 
 const config: Config = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts',
-  ],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
   transform: {
     '.+\\.ts$': 'ts-jest',
-  }
-};
+  },
+}
 
-export default config;
+export default config
